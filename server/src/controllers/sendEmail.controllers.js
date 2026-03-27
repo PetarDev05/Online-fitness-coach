ba767435e17992c92aff1cebd2c945add56e10fc
PetarDev05 <pejovicpetar02@gmail.com>
2026-03-27 17:05:53 +0100

    Add React-Email template

--- server/src/controllers/sendEmail.controllers.js
+++ server/src/controllers/sendEmail.controllers.js
@@ -1,18 +1,17 @@
 import { Resend } from "resend";
+import { render } from "@react-email/render";
+import ContactEmail from "../emails/ContactEmail.jsx";
 
 export const sendEmail = async (req, res) => {
   try {
     const { name, email, message } = req.body;
+    const html = render(ContactEmail({ name, email, message }));
     const resend = new Resend(process.env.RESEND_API_KEY);
     const { data, error } = await resend.emails.send({
-      from: "Petar <website@contact.fitcoach-demo.com>",
-      to: ["petar.webdev05@gmail.com"],
-      subject: "WEBSITE FORM TEST",
-      html: `<div>
-        <p>NAME: ${name}</p>
-        <p>EMAIL: ${email}</p>
-        <p>MESSAGE: ${message}</p>
-      </div>`,
+      from: `<${VERIFIED_DOMAIN}>`,
+      to: [`${TARGET_ADDRESS}`],
+      subject: "New Contact Form Message",
+      html,
     });
 
     if (error) {
@@ -22,7 +21,12 @@ export const sendEmail = async (req, res) => {
       });
     }
 
-    res.status(200).json({ message: "Message sent! Thanks for reaching out", status: "success" });
+    res
+      .status(200)
+      .json({
+        message: "Message sent! Thanks for reaching out",
+        status: "success",
+      });
   } catch (error) {
     res.status(400).json({
       message: "Something went wrong. Try again later",
